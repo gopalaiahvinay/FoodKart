1def0b1e0b0a10275a49d075a0577fe2c932f462
gopalaiahvinay <gopalaiahvinay@gmail.com>
2015-08-23 12:50:37 -0400

    Commiting sidemenu

--- FoodKart/routes/mainRoutes.js
+++ FoodKart/routes/mainRoutes.js
@@ -1,8 +1,15 @@
 // Home Route
+
+Router.configure({
+  layoutTemplate: 'basicLayout',
+  notFoundTemplate: 'notFound',
+  loadingTemplate: 'loading'
+});
+
 Router.route('/', {
   name: 'home',
   action: function () {
     this.render('home');
-    SEO.set({ title: 'Home - ' + Meteor.App.NAME });
+    SEO.set({ title: 'Home - Food Kart' });
   }
 });
